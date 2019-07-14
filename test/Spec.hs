b0431ef2c362a9052449e02ac2423757ea05f751
0xd34df00d <0xd34df00d@gmail.com>
2019-07-14 19:46:15 -0400

    Stub test that it at least typechecks

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,31 @@
+{-# LANGUAGE DataKinds, GADTs #-}
+{-# LANGUAGE OverloadedStrings, StandaloneDeriving #-}
+
+import System.Remote.Monitoring
+
+import System.Metrics.Extensible
+
+data TestMetrics ty name where
+  Foo1 :: TestMetrics Counter "foo1"
+  Foo2 :: TestMetrics Counter "foo2"
+  Bar  :: TestMetrics Gauge   "bar"
+
+deriving instance Eq (TestMetrics ty name)
+deriving instance Ord (TestMetrics ty name)
+
+data OtherMetrics ty name where
+  Baz  :: OtherMetrics Counter "foo1"
+  Quux :: OtherMetrics Gauge   "bar"
+
+deriving instance Eq (OtherMetrics ty name)
+deriving instance Ord (OtherMetrics ty name)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  ekgServer <- forkServer "localhost" 21000
+  withMetricsStore ekgServer $ \metrics -> flip runMetricsT metrics $ do
+    track Foo1
+    track Foo2
+    track Bar 10
+    track Baz
+    track Quux 42
