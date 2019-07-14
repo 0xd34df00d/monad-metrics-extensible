f23bd0e1328c801a3d90aa25a3c4066d28b89a69
0xd34df00d <0xd34df00d@gmail.com>
2019-07-14 19:39:30 -0400

    Reexport EKG trackers types as well

--- src/System/Metrics/Extensible.hs
+++ src/System/Metrics/Extensible.hs
@@ -2,6 +2,11 @@ module System.Metrics.Extensible
 ( module X
 ) where
 
+import System.Metrics.Counter as X(Counter)
+import System.Metrics.Distribution as X(Distribution)
+import System.Metrics.Gauge as X(Gauge)
+import System.Metrics.Label as X(Label)
+
 import System.Metrics.Monad as X
 import System.Metrics.Monad.Class as X
 import System.Metrics.Store as X
