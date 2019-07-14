67957210eada9e76517125b946502002ec36a53e
0xd34df00d <0xd34df00d@gmail.com>
2019-07-14 19:23:47 -0400

    Added System.Metrics.Extensible that just reexports everything

---
+++ src/System/Metrics/Extensible.hs
@@ -0,0 +1,8 @@
+module System.Metrics.Extensible
+( module X
+) where
+
+import System.Metrics.Monad as X
+import System.Metrics.Monad.Class as X
+import System.Metrics.Store as X
+import System.Metrics.TrackerInstances as X
