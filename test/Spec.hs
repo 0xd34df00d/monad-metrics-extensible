47bf51df834a6c6ffa7ff10cd900c0db9aa09c3b
0xd34df00d <0xd34df00d@gmail.com>
2019-07-14 19:52:39 -0400

    Test that metrics are saved correctly

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE DataKinds, GADTs #-}
 {-# LANGUAGE OverloadedStrings, StandaloneDeriving #-}
 
+import Control.Monad.IO.Class
+import System.Metrics.Counter as TC(read)
+import System.Metrics.Gauge as TG(read)
 import System.Remote.Monitoring
+import Test.Hspec
 
 import System.Metrics.Extensible
 
@@ -23,9 +27,19 @@ deriving instance Ord (OtherMetrics ty name)
 main :: IO ()
 main = do
   ekgServer <- forkServer "localhost" 21000
-  withMetricsStore ekgServer $ \metrics -> flip runMetricsT metrics $ do
+  withMetricsStore ekgServer $ \store -> flip runMetricsT store $ do
+    track Foo1
     track Foo1
     track Foo2
     track Bar 10
+    track Bar 20
     track Baz
     track Quux 42
+    liftIO $ hspec $ do
+      describe "Counters are stored as expected" $ do
+        it "Foo1 is incremented twice" $ (getMetricFromStore store Foo1 >>= TC.read) `shouldReturn` 2
+        it "This does not affect Foo2" $ (getMetricFromStore store Foo2 >>= TC.read) `shouldReturn` 1
+        it "Metrics from other type are also saved" $ (getMetricFromStore store Baz >>= TC.read) `shouldReturn` 1
+      describe "Gauges are stored as expected" $ do
+        it "The final value of Bar is kept" $ (getMetricFromStore store Bar >>= TG.read) `shouldReturn` 20
+        it "Metrics from other type are also saved" $ (getMetricFromStore store Quux >>= TG.read) `shouldReturn` 42
