ddec190b1700b66e65766ee68e965929a1eb7282
0xd34df00d <0xd34df00d@gmail.com>
2021-01-29 12:09:38 -0600

    Throw up some tests for the timer

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,17 +2,22 @@
 {-# LANGUAGE OverloadedStrings, StandaloneDeriving #-}
 
 import Control.Monad.IO.Class
+import Data.Functor
+import System.CPUTime
 import System.Metrics.Counter as TC(read)
+import System.Metrics.Distribution as TD(read, sum, count)
 import System.Metrics.Gauge as TG(read)
 import System.Remote.Monitoring
 import Test.Hspec
 
 import System.Metrics.Extensible
+import System.Metrics.ExtraTrackers
 
 data TestMetrics ty name where
-  Foo1 :: TestMetrics Counter "foo1"
-  Foo2 :: TestMetrics Counter "foo2"
-  Bar  :: TestMetrics Gauge   "bar"
+  Foo1    :: TestMetrics Counter        "foo1"
+  Foo2    :: TestMetrics Counter        "foo2"
+  Bar     :: TestMetrics Gauge          "bar"
+  TimeMS  :: TestMetrics (Timer 'Msecs) "timeMS"
 
 deriving instance Eq (TestMetrics ty name)
 deriving instance Ord (TestMetrics ty name)
@@ -24,6 +29,14 @@ data OtherMetrics ty name where
 deriving instance Eq (OtherMetrics ty name)
 deriving instance Ord (OtherMetrics ty name)
 
+cpuThreadDelay :: Int -> IO ()
+cpuThreadDelay usecs | usecs < 0 = pure ()
+                     | otherwise = do
+  start <- getCPUTime
+  void $ readFile "/dev/null"
+  end <- getCPUTime
+  cpuThreadDelay $ usecs - fromIntegral ((end - start) `div` 1000000)
+
 main :: IO ()
 main = do
   ekgServer <- forkServer "localhost" 21000
@@ -34,6 +47,7 @@ main = do
     track Bar 10
     track Bar 20
     track Baz
+    timed TimeMS $ liftIO $ cpuThreadDelay testMicroseconds
     track Quux 42
     liftIO $ hspec $ do
       describe "Counters are stored as expected" $ do
@@ -43,3 +57,15 @@ main = do
       describe "Gauges are stored as expected" $ do
         it "The final value of Bar is kept" $ (getMetricFromStore store Bar >>= TG.read) `shouldReturn` 20
         it "Metrics from other type are also saved" $ (getMetricFromStore store Quux >>= TG.read) `shouldReturn` 42
+      describe "Timers are stored and computed as expected" $ do
+        it "TimeMS has only seen one value" $ do
+          storedStats <- getMetricFromStore store TimeMS >>= TD.read . getTimerDistribution
+          TD.count storedStats `shouldBe` 1
+        it "The value of TimeMS exceeds the wait time" $ do
+          storedStats <- getMetricFromStore store TimeMS >>= TD.read . getTimerDistribution
+          TD.sum storedStats `shouldSatisfy` (>= (fromIntegral testMicroseconds / 1000))
+        it "…but not too much" $ do
+          storedStats <- getMetricFromStore store TimeMS >>= TD.read . getTimerDistribution
+          TD.sum storedStats `shouldSatisfy` (< 1000 * (fromIntegral testMicroseconds / 1000))
+  where
+    testMicroseconds = 10000
